448a5d25c0d7028dabd68ff59c5241a10f024260
Ghilke <minefap44@gmail.com>
2020-07-16 00:17:57 +0200

    Interactable

---
+++ Project E/Assets/Chest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : Interactable
+{
+
+    int[] weights = { 60, 30, 10 };
+
+    protected override void Awake()
+    {
+        base.Awake();
+        interactionTransform = transform.GetChild(0);
+    }
+
+    protected override void Interact()
+    {
+        base.Interact();
+        int odd = Random.Range(0, 100);
+        if(odd < 60)
+        {
+            Debug.Log("Found coins");
+        }
+        else
+        {
+            odd = odd - 60;
+        }
+
+    }
+
+}
---
+++ Project E/Assets/Interactable.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+
+    protected Transform interactionTransform = default;
+
+    public float radius = 1f;
+
+    protected LayerMask playerLayer;
+
+    protected bool interacted = false;
+    // Start is called before the first frame update
+    protected virtual void Awake()
+    {
+        playerLayer = LayerMask.GetMask(LayerMask.LayerToName(9));
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+        if (Physics.CheckSphere(interactionTransform.position, radius, playerLayer) && Input.GetKeyDown(KeyCode.E))
+        {
+            if (!interacted)
+            {
+                Interact();
+            }
+            else
+            {
+                Debug.Log("Can't do that");
+            }
+            
+        }
+    }
+
+    protected virtual void Interact()
+    {
+        Debug.Log("Interact");
+        interacted = true;
+    }
+
+    protected virtual void OnDrawGizmosSelected()
+    {
+        interactionTransform = transform.GetChild(0);
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+}
